830ff10cb19f37e9a4680ccce9d0ce7da8f887f4
Sempr <iamsempr@gmail.com>
2013-04-02 12:07:48 +0800

    MOD: move streaming.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ __author__ = 'Sempr'
 
 setup(
     name='taobaopy',
-    version='2.0',
+    version='2.0.1',
     url='https://github.com/sempr/taobaopy',
     license='BSD',
     author='Fred Wang',
