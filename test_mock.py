a90afa397753324392514b43a6d5992d54bd2850
Sempr <iamsempr@gmail.com>
2020-06-30 09:41:16 +0800

    MOD: support upload image

--- test_mock.py
+++ test_mock.py
@@ -1,5 +1,13 @@
 import requests_mock
 from taobaopy.taobao import TaoBaoAPIClient, TaoBaoAPIError
+from io import BytesIO
+
+
+class NamedBytesIO(BytesIO):
+    def __init__(self, content, name):
+        self.name = name
+        self.length = len(content)
+        super(NamedBytesIO, self).__init__(content)
 
 
 def test_no_json():
@@ -30,3 +38,11 @@ def test_json_fix():
         c = TaoBaoAPIClient(app_key="key", app_secret="sec", domain="test.com", retry_count=1)
         res = c.time_get(a=123)
         assert res["time_get_response"]["time"] == '\n\r\t2000-01-01 00:00:00'
+
+
+def test_upload():
+    with requests_mock.Mocker() as m:
+        m.post("http://test.com/router/rest", text='{}')
+        c = TaoBaoAPIClient(app_key="key", app_secret="sec", domain="http://test.com", retry_count=1)
+        res = c.time_get(file1=NamedBytesIO(b"abc", "test.png"))
+        assert res == {}
